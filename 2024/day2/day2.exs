5e6ef9d65df66a181f8b2bb30a4253c2a206652a
aaronshahriari <aaronshahriari@gmail.com>
2024-12-10 05:53:46 -0500

    finished day 2 part 1

--- 2024/day2/day2.exs
+++ 2024/day2/day2.exs
@@ -1,12 +1,32 @@
 defmodule Day02 do
+  def increasing_adj?(list) do
+    Enum.chunk_every(list, 2, 1, :discard)
+    |> Enum.all?(fn [a, b] -> a < b and abs(a - b) >= 1 and abs(a - b) <= 3 end)
+  end
+
+  def decreasing_adj?(list) do
+    Enum.chunk_every(list, 2, 1, :discard)
+    |> Enum.all?(fn [a, b] -> a > b and abs(a - b) >= 1 and abs(a - b) <= 3 end)
+  end
+
   # run case statement that parses the list
   def part1(input) do
     all = parse(input)
-    all
-  end
 
-  # def part2(input) do
-  # end
+    result =
+      Enum.map(all, fn list ->
+        cond do
+          decreasing_adj?(list) or increasing_adj?(list) ->
+            "SAFE"
+
+          true ->
+            "NOT SAFE"
+        end
+      end)
+
+    count = Enum.count(result, &(&1 == "SAFE"))
+    count
+  end
 
   defp parse(input) do
     input
@@ -17,5 +37,5 @@ defmodule Day02 do
   end
 end
 
-Day02.part1(File.stream!("day2.test", :line))
+Day02.part1(File.stream!("day2.input", :line))
 |> IO.inspect()
